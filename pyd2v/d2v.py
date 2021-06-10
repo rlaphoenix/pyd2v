8ac31ca3396d4c1290c2bbf135b749289addb46b
PHOENiX <rlaphoenix@pm.me>
2021-06-10 11:28:35 +0100

    D2V: Optimize Header data code

--- pyd2v/d2v.py
+++ pyd2v/d2v.py
@@ -17,20 +17,22 @@ class D2V:
         # Data
         self.data = None
         self.data_type = None
+
         with open(filename, mode="r", encoding="utf-8") as f:
-            # Version
+
+            """ Header Data """
+
             self.version = f.readline().strip()
             if not self.version.startswith("DGIndexProjectFile"):
-                raise ValueError(f"Expected Version Header, received:\n\t{self.version}")
-            self.version = self.version[18:]  # strip "DGIndexProjectFile"
-            # Videos
-            self.videos = []
-            for n in range(int(f.readline().strip())):
-                self.videos.append(f.readline().strip())
-            # Headers Terminate Check
+                raise ValueError(f"Expected Version Header, received '{self.version}'")
+            self.version = int(self.version[18:])  # strip "DGIndexProjectFile"
+
+            self.videos = [f.readline().strip() for _ in range(int(f.readline().strip()))]
+
             if len(f.readline().strip()) > 0:
                 raise ValueError("Unexpected data after reading Header's Video List.")
             # Settings
+
             self.settings = {}
             int_list = ["Stream_Type", "MPEG_Type", "iDCT_Algorithm", "YUVRGB_Scale", "Field_Operation"]
             while True:
